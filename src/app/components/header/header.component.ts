ae431f58bee346df0e8c10c6ce3fc50f299fa794
metin-gercek <metingercek@gmail.com>
2025-05-08 10:21:59 +0300

    create header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  title = input.required<string>();
+
+}
