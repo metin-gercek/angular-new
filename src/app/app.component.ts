c46e40b7a4ddb3137b364c6783086a2af1c758a3
metin-gercek <metingercek@gmail.com>
2025-05-11 12:57:53 +0300

    add counter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { CounterComponent } from './components/counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CounterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
